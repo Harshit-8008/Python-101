74c22fe2c319a8d257134eaf94bc1d6528cab7a2
Harshit <2019UGCS016@nitjsr.ac.in>
2022-03-07 22:45:17 +0530

    Updated README.md

--- main.py
+++ main.py
@@ -15,9 +15,32 @@ print(multi_line)
 # print("hello")
 # b=chr(97)
 # print(b)
-print("Enter x")
-x=int(input())
-print("Enter y")
-y=int(input())
-print("And the sum is equal to ->",x+y,sep=" ")
+# print("Enter x")
+# x=int(input())
+# print("Enter y")
+# y=int(input())
+# print("And the sum is equal to ->",x+y,sep=" ")
+#conditional statements
+# x=int(input())
+# if x<10 :
+#     print("Not ok")
+# elif x>=10 and x<20 :
+#     print("somewhat ok")
+# else :
+#     print("Totally ok")
+# for i in range(1,11):
+#     if i%2==0:
+#         print(i,end=" ")
+# i=15
+# while i>=0:
+#     print(i,end=" ")
+#     i-=1
+
+# print("Hello World")
+# import numpy as np
+# # from PIL import Image
+# # with Image.open("sample.jpg") as im:
+# #     im.rotate(45).show()
+# arr=np.zeros((2,3))
+# print(arr)
 
