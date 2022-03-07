579b2ec97a4e41e7d2bca30b9628af4c482a4403
Harshit <2019UGCS016@nitjsr.ac.in>
2022-03-07 11:11:53 +0530

    Updated README.md

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+print("Hello World")
+print("I am Soham Mukherjee")
+print(10,20,30,sep=" ")
+print("-----------------------------------")
+x=10
+y=25
+z=x+y
+print(z)
+"""
+""""
+
+print(multi_line)
+"""
+# print("hello")
+# b=chr(97)
+# print(b)
+print("Enter x")
+x=int(input())
+print("Enter y")
+y=int(input())
+print("And the sum is equal to ->",x+y,sep=" ")
+
